34d91c94fa78d79a4834ed9065830ec4717fde4a
adek\developer.amit <developer.amit@adek.gov.ae>
2024-07-13 19:48:34 +0530

    merge with main

--- ibc/src/pages/ContactUs.jsx
+++ ibc/src/pages/ContactUs.jsx
@@ -97,7 +97,7 @@ export default function ContactUs() {
               <p className='text-base flex tracking-tight text-gray-400 text-left mt-7'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#1EE547" className="w-5 h-5">
                 <path fillRule="evenodd" d="M2 3.5A1.5 1.5 0 013.5 2h1.148a1.5 1.5 0 011.465 1.175l.716 3.223a1.5 1.5 0 01-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 006.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 011.767-1.052l3.223.716A1.5 1.5 0 0118 15.352V16.5a1.5 1.5 0 01-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 012.43 8.326 13.019 13.019 0 012 5V3.5z" clipRule="evenodd" />
               </svg>
-                <a href="tel:09867622683" className="hover:text-green-500 ... ml-2">+91-9867622683</a>
+                <a href="tel:9820496834" className="hover:text-green-500 ... ml-2">+91-9820496834</a>
               </p>
 
               <p className='text-base flex tracking-tight text-gray-400 text-left mt-7'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#1E88E5" className="w-5 h-5">
